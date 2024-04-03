81839d91bc2411ad53ce326ea7d940e5b520f973
Peter <phellwege1@gmail.com>
2024-04-03 07:56:37 -0600

    added gif

--- src/loading/LoadingPage.js
+++ src/loading/LoadingPage.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import Typewriter from 'typewriter-effect';
+import Thinking from './Thinking.gif'
 import './loadingPage.css'
 export default function LoadingPage() {
   return (
     <div className='loadingPageWrapper'>
+        <h1>Thinking</h1>
         <div>
-
-        <Typewriter
-        options={{
-            strings: ['Give Us A Moment, While We Look Over The Property', 'This may take up to a minute'],
-            autoStart: true,
-            loop: true,
-        }}
-        />
+        <img src={Thinking} alt='thinking gif' id='loadingGif'/>
+            <Typewriter
+            options={{
+                strings: ['Give Us A Moment While We Look Over The Property', 'This May Take Up To A Minute'],
+                autoStart: true,
+                loop: true,
+            }}
+            />
         </div>
     </div>
   )
